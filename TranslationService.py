22abbc1167aa97f280c40204a7818c8e2a20e94d
NaVo1319 <70800751+NaVo1319@users.noreply.github.com>
2023-02-12 16:07:36 +0300

    exp1

--- TranslationService.py
+++ TranslationService.py
@@ -1,33 +1,19 @@
 from http.server import BaseHTTPRequestHandler
-from http.server import HTTPServer
-import json
-import translators as ts # Переводим текст
-import time
+from urllib import parse
 
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-  server_address = ('', 8000)
-  httpd = server_class(server_address, handler_class)
-  try:
-      httpd.serve_forever()
-  except KeyboardInterrupt:
-      httpd.server_close()
-class HttpGetHandler(BaseHTTPRequestHandler):
-    """Обработчик с реализованным методом do_GET."""
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write('<html><head><meta charset="utf-8">'.encode())
-        self.wfile.write('<title>Простой HTTP-сервер.</title></head>'.encode())
-        self.wfile.write('<body>Был получен GET-запрос.</body></html>'.encode())
-    def do_POST(self):
+class handler(BaseHTTPRequestHandler):
 
-        content_len = self.rfile.read(int(self.headers.get('Content-Length')))
-        content = json.loads(content_len)
-        mytext = ts.google(content['text'], from_language=content['from'], to_language=content['to'])
-        self.red.set(int(time.time()),content['text'])
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(mytext.encode())
-run(handler_class=HttpGetHandler)
+	def do_GET(self):
+		s = self.path
+		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+		self.send_response(200)
+		self.send_header('Content-type','text/plain')
+		self.end_headers()
+
+		if "name" in dic:
+			message = "Hello, " + dic["name"] + "!"
+		else:
+			message = "Hello, stranger!"
+
+		self.wfile.write(message.encode())
+		return
