8c3ebfd84105a4fa4e4fc04c4f40a508839e4852
NaVo1319 <70800751+NaVo1319@users.noreply.github.com>
2023-02-13 12:45:45 +0300

    Add files via upload

---
+++ api/audio.py
@@ -0,0 +1,27 @@
+
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import json
+import time
+from gtts import gTTS # Озвучиваем текст
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        message = "server is ready"
+        self.wfile.write(message.encode())
+        return
+    def do_POST(self):
+        content_len = self.rfile.read(int(self.headers.get('Content-Length')))
+        content = json.loads(content_len)
+        self.red.set(int(time.time()), content['text'])
+        file = gTTS(text=content['text'], lang=content['lang'], slow=False)
+        file.save("speech.mp3")
+        self.send_response(200)
+        self.send_header("Content-type", "audio/mpeg")
+        self.end_headers()
+        with open('./speech.mp3', 'rb') as file:
+            self.wfile.write(file.read())
