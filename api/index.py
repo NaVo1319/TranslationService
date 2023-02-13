7ffc0d84055f1744f702cd10f4c011ad5285761d
NaVo1319 <70800751+NaVo1319@users.noreply.github.com>
2023-02-13 12:10:46 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,22 +1,23 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-
+import json
+import translators as ts # Переводим текст
+import time
 class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
 
-	def do_GET(self):
-		s = self.path
-		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-		self.send_response(200)
-		self.send_header('Content-type','text/plain')
-		self.end_headers()
+        if "name" in dic:
+            message = "Hello, " + dic["name"] + "!"
+        else:
+            message = "Hello, stranger!"
 
-		if "name" in dic:
-			message = "Hello, " + dic["name"] + "!"
-		else:
-			message = "Hello, stranger!"
-
-		self.wfile.write(message.encode())
-		return
+        self.wfile.write(message.encode())
+        return
     def do_POST(self):
         content_len = self.rfile.read(int(self.headers.get('Content-Length')))
         content = json.loads(content_len)
