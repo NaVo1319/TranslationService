c6dc2a9d94fecf68de890e2a94da7fbf898baef1
NaVo1319 <70800751+NaVo1319@users.noreply.github.com>
2023-02-13 12:51:25 +0300

    Update audio.py

--- api/audio.py
+++ api/audio.py
@@ -18,9 +18,7 @@ class handler(BaseHTTPRequestHandler):
         content_len = self.rfile.read(int(self.headers.get('Content-Length')))
         content = json.loads(content_len)
         file = gTTS(text=content['text'], lang=content['lang'], slow=False)
-        file.save("speech.mp3")
         self.send_response(200)
         self.send_header("Content-type", "audio/mpeg")
         self.end_headers()
-        with open('./speech.mp3', 'rb') as file:
-            self.wfile.write(file.read())
+        self.wfile.write(file.read())
