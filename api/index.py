31bec707af543a0a5567bebc3bd92eb3ce350af5
NaVo1319 <70800751+NaVo1319@users.noreply.github.com>
2023-02-13 12:37:17 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -21,7 +21,6 @@ class handler(BaseHTTPRequestHandler):
         content_len = self.rfile.read(int(self.headers.get('Content-Length')))
         content = json.loads(content_len)
         mytext = ts.google(content['text'], from_language=content['from'], to_language=content['to'])
-        self.red.set(int(time.time()),content['text'])
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
